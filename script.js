1e921db0d0b0370bb34ab6487307b0fda58b6c3c
Awes Khan <44608315+Awes-Khan@users.noreply.github.com>
2024-08-22 00:22:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
+	let fname = document.getElementsByName("fname")[0];
+	let lname = document.getElementsByName("lname")[0];
+	alert(fname + ' ' + lname);
 
 }
